9ba0674bb4970a035cecf529d3c1504da54ccf95
Manuel Gomez <mgeducation.ia2@gmail.com>
2026-03-12 19:04:36 -0400

    Reconfiguración total de PWA: Estrategia automatizada generateSW y detección automática de actualizaciones

--- vite.config.js
+++ vite.config.js
@@ -9,12 +9,10 @@ export default defineConfig({
     react(),
     tailwindcss(),
     VitePWA({
-      strategies: 'injectManifest',
-      srcDir: 'src',
-      filename: 'sw.js',
       registerType: 'autoUpdate',
       injectRegister: 'auto',
-      includeAssets: ['vite.svg', 'pwa-192x192.png', 'pwa-512x512.png'],
+      strategies: 'generateSW',
+      includeAssets: ['favicon.ico', 'pwa-192x192.png', 'pwa-512x512.png', 'vite.svg'],
       manifest: {
         name: 'KPoint',
         short_name: 'KPoint',
@@ -27,7 +25,6 @@ export default defineConfig({
         start_url: '/',
         id: '/',
         lang: 'es',
-
         icons: [
           {
             src: 'pwa-192x192.png',
@@ -46,6 +43,29 @@ export default defineConfig({
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+        skipWaiting: true,
+        navigateFallback: 'index.html',
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
           }
         ]
       },
